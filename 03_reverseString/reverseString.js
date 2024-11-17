79ac35b9bb2d26dd2a0c90f579201d7aec342a14
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-11-17 18:17:40 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let startArray = string.split("");
+    let resultArray = [];
+    for (i = 0; i < string.length; i++) {
+        resultArray.push(startArray.pop());
+    }
+    return resultArray.join("");
 };
+
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
