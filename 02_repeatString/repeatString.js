460facacad29ea05e83b9d1b82c8f9e0d8236496
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-11-16 22:23:14 -0500

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = ""
+    if (num < 0) {
+        result = "ERROR";
+        return result;
+    } else {
+        for (i = 0; i < num; i++) {
+            result += string;
+        }
+        return result;
+    }
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
