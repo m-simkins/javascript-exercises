6ad4f7800497f97cb483ec8c18be8f86dd6c9446
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-11-19 22:52:15 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(tempF) {
+  let tempC = Math.round(((tempF - 32) / 1.8) * 10) / 10;
+  return tempC
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(tempC) {
+  let tempF = Math.round((tempC * 1.8 + 32) * 10) / 10;
+  return tempF;
+}
+
+convertToCelsius(32);
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
