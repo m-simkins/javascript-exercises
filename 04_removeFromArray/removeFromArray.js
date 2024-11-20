e9af82d859489463c720395afae8150e70bde2a7
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-11-19 21:48:42 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,31 @@
-const removeFromArray = function(array, toRemove) {
-    let position = array.indexOf(toRemove);
-    array.splice(position, 1);
-    return array;
-};
+const removeFromArray = function(array, ...toRemove) {
 
-removeFromArray([1, 2, 3, 4], 3);
+    for (i = 0; i < toRemove.length; i++) {
+        if (!array.includes(toRemove[i])) {
+            toRemove.splice(i, 1);    
+        }
+    }
+
+    const sortedArray = array.sort((a, b) => a - b);
+    const newArray = [];
+
+    for (i = 0; i < sortedArray.length; i++) {
+        if (sortedArray[i] !== sortedArray[i+1]) {
+            newArray.push(sortedArray[i]);
+        }
+    }
+
+    for (i = 0; i < toRemove.length; i++) {
+        if (newArray.includes(toRemove[i])) {
+            let position = newArray.indexOf(toRemove[i]);
+            newArray.splice(position, 1);    
+        }
+    }
+    
+    return newArray;
+}
+
+removeFromArray([1, 2, 3, 4], 2, 5);
 
 // Do not edit below this line
 module.exports = removeFromArray;
