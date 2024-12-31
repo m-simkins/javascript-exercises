49dd5bdce14af63994c88687f102667e11a2341a
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-12-31 09:36:13 -0500

    complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let array = [1, 1];
 
+    if (typeof n !== Number) {
+        n = Number(n);
+    }
+
+    if (n < 0) {
+        return "OOPS";
+    }
+    else if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {    
+        for (let i = 2; i < n; i++) {
+            array.push(array[i-1] + array[i-2]);
+        }
+        return array[n - 1];
+    }
 };
 
 // Do not edit below this line
