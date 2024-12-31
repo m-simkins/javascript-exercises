e8a3c339f54d6f629805259e4260f4ac0950f683
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-12-30 21:05:21 -0500

    complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
 const palindromes = function (str) {
-}
+    const validator = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+    const word = str
+                    .toLowerCase()
+                    .split("")
+                    .filter((char) => validator.includes(char))
+                    .join("");
+    const reversed = word.split("").reverse().join("");
+    console.log(word);
+    console.log(reversed);
+    return word === reversed;
+};
+
 // Do not edit below this line
 module.exports = palindromes;
