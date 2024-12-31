6912f8a0b68814efd6cd7317167dcf23266aec8a
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-12-31 09:57:18 -0500

    complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const thisYear = new Date().getFullYear();
+    people.forEach(person => {
+        if (!person.yearOfDeath) {
+            person.age = thisYear - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+    people.sort((a,b) => b.age - a.age);
+    return people[0];
 };
 
 // Do not edit below this line
